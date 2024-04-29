72c7144f9276d803f4fd9ede98842e07b0c0a097
kenh21 <kenneth.hauter@gmail.com>
2024-04-29 17:11:15 +0900

    Add function for computerChoice of rock papar scissors
    added in a fuction that returns rock, paper or
    scissors at random.

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3)+1;
+    if (choice <= 1) {
+        choice = 'Rock';
+    } else if (choice <= 2) {
+        choice = 'Paper';
+    } else  {
+        choice = 'Scissors';
+    }
+        console.log(choice)
+}
+getComputerChoice()
+
